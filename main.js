c8be82d48ab1dd32826ce42d037be4932e9b98aa
C Bares <colinedwinbares@gmail.com>
2021-10-27 18:11:43 -0500

    "Adding HTML"

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let verbs = ['cross','slap','choke','punch','smell','fight','torch','drink','sni
 let nouns = ['road','farmer','referee','clock','iceberg','grandma','Chrysler','Pepsi','afro','ATM','numbers','riddle'];
 let punchlines = ['To get to the other side!','Because he smelled!','Because he was called a foul!','Because he was working class!','To see if it was made of Old Spice!','Because she was so old!','Insurance fraud!','Because they were all out of Mountain Dew!','Because it was big!','Because he was a little light on cash!','To see how it all added up!','Because he was so smart and knew the answer so he solved it!'];
 
+// Function to generate a set up from various arrays.
 function setupMaker () {
     let animal = animals[Math.floor(Math.random() * 12)]
     let verb = verbs[Math.floor(Math.random() * 12)]
@@ -11,6 +12,7 @@ function setupMaker () {
     return `Why did the ${animal} ${verb} the ${noun}?`;
 }
 
+// Function to generate a punchline.
 function punchlineMaker() {
     let punchline = punchlines[Math.floor(Math.random() *12)]
     return punchline;
